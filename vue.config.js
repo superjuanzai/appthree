48d23f893604aa23e87f31a542af7e72b4a0a719
卷仔 <2682761705@qq.com>
2023-09-18 20:31:39 +0800

    sev

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/appthree/',
+  publicPath: './',
   lintOnSave: false,
 })
